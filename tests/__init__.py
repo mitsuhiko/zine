59df7a71e45d7376c4d15a8ec428137a3e7287cd
mq <devnull@localhost>
2008-07-07 12:17:09 +0200

    fixes in the test suite; add support for coverage.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,22 +10,53 @@
 """
 
 import sys
-from os import walk
+import os
 from os.path import join, dirname
-from unittest import TestSuite, main
+from unittest import TestSuite, TextTestRunner
 from doctest import DocTestSuite
 
 from textpress.application import make_textpress
 
-def suite():
+#: the modules in this list are not tested in a full run
+untested = ['textpress.i18n.compilejs']
+
+try:
+    import coverage
+except ImportError:
+    coverage = None
+
+
+def suite(return_covermods=False, modnames=[]):
     """Generate the test suite."""
     instance_path = join(dirname(__file__), 'instance')
     app = make_textpress(instance_path)
 
+    if return_covermods:
+        covermods = []
     suite = TestSuite()
-    for mod in find_tp_modules():
-        suite.addTest(DocTestSuite(mod, extraglobs={'app': app}))
-    return suite
+
+    if modnames == []:
+        modnames = find_tp_modules()
+    for modname in modnames:
+        if modname in untested:
+            continue
+        # currently there are problems with plugins because of the
+        # import hook. Therefore, we skip them:
+        if 'plugins' in modname:
+            continue
+        # the fromlist must contain something, otherwise the textpress
+        # package is returned, not our module
+        mod = __import__(modname, fromlist=[''])
+
+        dts = DocTestSuite(mod, extraglobs={'app': app})
+        if dts.countTestCases():
+            suite.addTest(dts)
+            if return_covermods:
+                covermods.append(mod)
+    if return_covermods:
+        return suite, covermods
+    else:
+        return suite
 
 
 def find_tp_modules():
@@ -33,29 +64,59 @@ def find_tp_modules():
     modules = []
     import textpress
     base = dirname(textpress.__file__)
-    start = len(dirname(base)) + 1
+    start = len(dirname(base))
+    if base != 'textpress':
+        start += 1
 
-    for path, dirnames, filenames in walk(base):
+    for path, dirnames, filenames in os.walk(base):
         for filename in filenames:
             if filename.endswith('.py'):
                 fullpath = join(path, filename)
-                # currently there are problems with plugins because of the
-                # import hook. Therefore, we skip them:
-                if 'plugins' in fullpath:
-                    continue
-
                 if filename == '__init__.py':
                     stripped = fullpath[start:-12]
                 else:
                     stripped = fullpath[start:-3]
 
                 modname = stripped.replace('/', '.')
-                # the fromlist must contain something, otherwise the textpress
-                # package is returned, not our module
-                modules.append(__import__(modname, fromlist=['']))
+                modules.append(modname)
     return modules
 
 
+def main():
+    from optparse import OptionParser
+    usage = ('Usage: %prog [option] [modules to be tested]\n'
+             'Modules names have to be given in the form utils.mail (without '
+             'textpress.)\nIf no module names are given, all tests are run')
+    parser = OptionParser(usage=usage)
+    parser.add_option('-c', '--coverage', action='store_true', dest='coverage',
+                      help='show coverage information (slow!)')
+    parser.add_option('-v', '--verbose', action='store_true', dest='verbose',
+                      default=False, help='show which tests are run')
+
+    options, args = parser.parse_args(sys.argv[1:])
+    modnames = ['textpress.' + modname for modname in args]
+    if options.coverage:
+        if coverage is not None:
+            use_coverage = True
+        else:
+            sys.stderr.write("coverage information requires Ned Batchelder's "
+                             "coverage.py to be installed!\n")
+            sys.exit(1)
+    else:
+        use_coverage = False
+
+    if use_coverage:
+        s, covermods = suite(True, modnames=modnames)
+        coverage.erase()
+        coverage.start()
+    else:
+        s = suite(modnames=modnames)
+    TextTestRunner(verbosity=options.verbose + 1).run(s)
+    if use_coverage:
+        coverage.stop()
+        print '\n\n' + '=' * 25 + ' coverage information ' + '=' * 25
+        coverage.report(covermods)
+
 
 if __name__ == '__main__':
-    main(defaultTest='suite')
+    main()
