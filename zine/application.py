ace2cea763506fb13e6b41a0d5ddca82e5167013
mq <devnull@localhost>
2008-12-22 03:21:38 +0100

    fix crash when using a localized zine

--- zine/application.py
+++ zine/application.py
@@ -778,7 +778,6 @@ class Zine(object):
                                'application setup phase.')
         self.__dict__.update(dict.fromkeys(self._setup_only, _error))
 
-        self.cfg.config_vars['language'].choices = i18n.list_languages()
         self.cfg.config_vars['default_parser'].choices = \
             self.cfg.config_vars['comment_parser'].choices = \
             self.list_parsers()
