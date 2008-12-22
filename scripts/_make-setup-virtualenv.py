1bf1d1b9b371f1b170b950b57888679fb99a1a1d
mitsuhiko <devnull@localhost>
2008-12-22 21:44:48 +0100

    Fixed lxml build for OS X in "setup-virtualenv".

--- scripts/_make-setup-virtualenv.py
+++ scripts/_make-setup-virtualenv.py
@@ -18,13 +18,13 @@ import os
 import sys
 from subprocess import call
 
+# requirements without lxml, because lxml is special for OS X
 REQUIREMENTS = [
     'Werkzeug>=0.4',
     'Jinja2>=2.1',
     'SQLAlchemy==dev',
     'pytz',
-    'Babel>=0.9.4',
-    'lxml>=2.0'
+    'Babel>=0.9.4'
 ]
 
 # for python 2.4/2.5 we want simplejson installed too.
@@ -33,23 +33,38 @@ if sys.version_info < (2, 6):
 
 # os x has some problems with lxml, make sure the user has port installed
 # so that we can compile lxml
+lxml_static_deps=False
 if sys.platform == 'darwin':
     print '=' * 60
     print 'It appears that you are using OS X.  If an installation error'
     print 'occurs on installing lxml, please make sure you have port'
-    print 'installed and execute the following command:'
-    print
-    print '    $ port install libxml2 libxslt'
+    print 'installed.
     print '=' * 60
 
-def install(home_dir, *args):
-    call([os.path.join(home_dir, 'bin', 'easy_install')] + list(args))
+    # no idea if that actually helps, but let's hope it does :D
+    _dyld_path = os.environ.get('DYLD_LIBRARY_PATH', '')
+    if _dyld_path:
+        _dyld_path += ':'
+    _dyld_path += '/opt/local/lib'
+    os.environ['DYLD_LIBRARY_PATH']= _dyld_path
+    lxml_static_deps = True
+
+def install(home_dir, *args, **kw):
+    static_deps = kw.pop('static_deps', False)
+    if kw:
+        raise TypeError('too many keyword arguments')
+    env = None
+    if static_deps:
+        env = dict(os.environ.items())
+        env['STATIC_DEPS'] = 'true'
+    call([os.path.join(home_dir, 'bin', 'easy_install')] + list(args), env=env)
 
 def after_install(options, home_dir):
     site_packages = os.path.normpath(os.path.join(home_dir, 'lib', 'python%%d.%%d'
         %% sys.version_info[:2], 'site-packages'))
     for requirement in REQUIREMENTS:
         install(home_dir, requirement)
+    install(home_dir, 'lxml>=2.0', static_deps=lxml_static_deps)
     call(['ln', '-s', %(zine_path)r, os.path.join(site_packages, 'zine')])
 '''
 
