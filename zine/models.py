4b2a35d748134285fe5ae336f49fbf8f13ad2d49
mitsuhiko <devnull@localhost>
2009-01-11 01:17:52 +0100

    Reverted a change that caused slugs to be prefixed with zeros.

--- zine/models.py
+++ zine/models.py
@@ -548,7 +548,7 @@ class Post(_ZEMLDualContainer):
         prefix = get_application().cfg['blog_url_prefix'].lstrip('/')
         if prefix:
             prefix += '/'
-        full_slug = u'%s%04d/%02d/%02d/%s' % (
+        full_slug = u'%s%04d/%d/%d/%s' % (
             prefix,
             self.pub_date.year,
             self.pub_date.month,
