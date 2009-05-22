df671fcf33f81f9e09fd96c22f4ae4080abd9d94
Kiran Jonnalagadda <jace@pobox.com>
2009-05-23 00:35:30 +0530

    Markdown parser now has an options page, for configuring extensions and for enabling the (still experimental) support for intro sections.

--- zine/plugins/markdown_parser/__init__.py
+++ zine/plugins/markdown_parser/__init__.py
@@ -11,13 +11,61 @@
     :copyright: (c) 2009 by the Zine Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
+import os.path
+import re
 from zine.api import *
 from zine.parsers import BaseParser
+from zine.views.admin import flash, render_admin_response
+from zine.privileges import BLOG_ADMIN, require_privilege
 from zine.utils.zeml import parse_html
+from zine.utils import forms
 try:
     import markdown as md
 except ImportError:
     from zine.plugins.markdown_parser import local_markdown as md
+
+TEMPLATES = os.path.join(os.path.dirname(__file__), 'templates')
+CFG_EXTENSIONS='markdown_parser/extensions'
+CFG_MAKEINTRO='markdown_parser/makeintro'
+MORE_TAG = re.compile(r'\n<!--\s*more\s*-->\n(?u)')
+
+
+class ConfigurationForm(forms.Form):
+    """Markdown configuration form."""
+    extensions = forms.LineSeparated(forms.TextField(),
+                                                    _(u'Enabled Extensions'))
+    makeintro = forms.BooleanField(_(u'Make Intro Section'),
+        help_text=_(u'Place <!--more-> on a line by itself with blank lines '\
+                    u'above and below to cut the post at that point.'))
+
+
+@require_privilege(BLOG_ADMIN)
+def show_markdown_config(req):
+    """Show Markdown Parser configuration options."""
+    form = ConfigurationForm(initial=dict(
+                                    extensions=req.app.cfg[CFG_EXTENSIONS],
+                                    makeintro=req.app.cfg[CFG_MAKEINTRO]))
+
+    if req.method == 'POST' and form.validate(req.form):
+        if form.has_changed:
+            cfg = req.app.cfg.edit()
+            cfg[CFG_EXTENSIONS] = form['extensions']
+            cfg[CFG_MAKEINTRO] = form['makeintro']
+            cfg.commit()
+            flash(_('Markdown Parser settings saved.'), 'ok')
+    return render_admin_response('admin/markdown_options.html',
+                                 'options.markdown',
+                                 form=form.as_widget())
+
+
+def add_config_link(req, navigation_bar):
+    """Add a link to the Markdown options page"""
+    if req.user.has_privilege(BLOG_ADMIN):
+        for link_id, url, title, children in navigation_bar:
+            if link_id == 'options':
+                children.insert(2, ('markdown',
+                                    url_for('markdown_parser/config'),
+                                    _('Markdown')))
 
 
 class MarkdownParser(BaseParser):
@@ -26,12 +74,28 @@ class MarkdownParser(BaseParser):
     name = _(u'Markdown')
 
     def parse(self, input_data, reason):
+        cfg = get_application().cfg
         parser = md.Markdown(safe_mode=reason == 'comment' and 'escape',
-                             extensions=['codehilite'],
+                             extensions=cfg[CFG_EXTENSIONS],
+                             #: For compatibility with the Pygments plugin
                              extension_configs={'codehilite':
                                                     {'css_class': 'syntax'}})
-        return parse_html(parser.convert(input_data))
+        html = parser.convert(input_data)
+        if cfg[CFG_MAKEINTRO]:
+            if MORE_TAG.search(html):
+                #: Crude hack, but parse_html will correct any html
+                #: closure errors we introduce
+                html = u'<intro>' + MORE_TAG.sub(u'</intro>', html, 1)
+        return parse_html(html)
 
 
 def setup(app, plugin):
     app.add_parser('markdown', MarkdownParser)
+    app.add_config_var(CFG_EXTENSIONS,
+                       forms.LineSeparated(forms.TextField(), default=[]))
+    app.add_config_var(CFG_MAKEINTRO, forms.BooleanField(default=False))
+    app.connect_event('modify-admin-navigation-bar', add_config_link)
+    app.add_url_rule('/options/markdown', prefix='admin',
+                     endpoint='markdown_parser/config',
+                     view=show_markdown_config)
+    app.add_template_searchpath(TEMPLATES)
